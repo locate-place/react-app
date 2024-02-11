e94360880ab6f01084c339590a50e584fd3d8693
Björn Hempel <bjoern@hempel.li>
2024-02-11 12:01:30 +0100

    Refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,12 @@ import React from "react";
 
 /* Import test libraries. */
 import { render, screen } from '@testing-library/react';
+
+/* jest-dom adds custom jest matchers for asserting on DOM nodes.
+ * allows you to do things like:
+ * expect(element).toHaveTextContent(/react/i)
+ * learn more: https://github.com/testing-library/jest-dom
+ */
 import '@testing-library/jest-dom';
 
 import App from './App';
